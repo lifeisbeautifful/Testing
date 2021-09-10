ad8a2ff1eeca565533e006dd2bad5402ca93ed2c
lifeisbeautifful <42445537+lifeisbeautifful@users.noreply.github.com>
2021-09-10 10:03:42 +0300

    new cga

--- EAAPP/EAAPP/Drivers.cs
+++ EAAPP/EAAPP/Drivers.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EAAPP
 {
-    class Drivers
+    public class Drivers
     {
+        private IWebDriver Driver { get; set; }
+
+        public enum Browsers
+        {
+            Chrome,
+            FireFox
+        }
+
+        public void ChooseDriver(Browsers br)
+        {
+            switch (br)
+            {
+                case Browsers.Chrome:
+                    ChromeOptions option = new ChromeOptions();
+                    option.AddArgument("--headless");
+                    //add
+                    Driver = new ChromeDriver(option);
+                    break;
+                case Browsers.FireFox:
+                    FirefoxOptions optionFF = new FirefoxOptions();
+                    optionFF.AddArgument("--headless");
+                    Driver = new FirefoxDriver(optionFF);
+                    break;
+            }
+        }
+
+        public void Navigate(string url)
+        {
+            Driver.Navigate().GoToUrl(url);
+        }
     }
 }
